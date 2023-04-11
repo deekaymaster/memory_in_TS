cb9470004f3b9f2c5170dc9c9eac4825c082dcbc
Dawid Kogut <kogutdawid@gmail.com>
2023-04-11 22:08:34 +0200

    add bootstrap 5 to project and block double click

--- src/classes/MemoryGame.ts
+++ src/classes/MemoryGame.ts
@@ -1,7 +1,7 @@
 interface Card {
     id: number;
-    color: string;
-    flipped: boolean;
+    image: string;
+    obverse: boolean; // awers
   }
 
   interface PickedCard {
@@ -9,21 +9,38 @@ interface Card {
     pickedCard: HTMLDivElement;
   }
 
+// na późniejszym etapie spróbuje pobrać zawartość folderu z grafikami AJAXEM
+// na razie robimy tak ze losuje z tej tablicy do tablicy pomocniczej potrzebna liczbe zdjec
 export class MemoryGame {
     private _cards: Card[];
     private _numberOfColumns: number;
     private _numberOfRows: number;
-    private _colors: string[];
+    private _graphics: string[] = [
+        "1.png",
+        "2.png",
+        "3.png",
+        "4.png",
+        "5.png",
+        "6.png",
+        "7.png",
+        "8.png"
+    ];
     private _numberOfPickedCards: number = 0;
     private _pickedCards: PickedCard[] = [];
-    private _canPicking: boolean = true;
+    private _attempts: number = -1;
 
     constructor(numberOfColumns: number, numberOfRows: number){
+        this.blockOptionInputs(); // blokujemy możliwość wyboru rozmiaru planszy
         this._numberOfColumns = numberOfColumns;
         this._numberOfRows = numberOfRows;
-        this._colors = this.drawColors((numberOfColumns*numberOfRows)/2);
-        this._colors = this._colors.concat(this._colors); // dublowanie zawartości tablicy
-        this.shuffleArray(); // mieszanie zawartości tablicy z kolorami
+        // losowanie potrzebnej ilości grafik z folderu
+        let tempArray: string[] = [];
+        for(let i=0; i<(numberOfColumns*numberOfRows)/2; i++)
+        {
+            tempArray.push(this._graphics[i]);
+        }
+        this._graphics = tempArray.concat(tempArray); // dublowanie zawartości tablicy
+        this.shuffleArray(); // mieszanie zawartości tablicy
         this._cards = this.createCards();
         this.renderBoard();
     }
@@ -34,23 +51,33 @@ export class MemoryGame {
 
     renderBoard = () => {
 
+        this.attemptIncrement();
+
         const board: HTMLDivElement = document.querySelector("#board");
-        board.innerHTML = '';
+        let rowForCards = document.createElement("div");
 
         this._cards.forEach(card => {
-    
-            let cardElement = document.createElement("div");
+
+            const cardElement = document.createElement("div");
             cardElement.id = card.id.toString();
-            
-            cardElement.setAttribute("name", card.color);
-            // console.log(`kolor: ${card.color}`);
-            cardElement.classList.add("reverse");
-            cardElement.classList.add('card');
+            cardElement.classList.add("reverse", "card", "col-3");
+            cardElement.setAttribute("name", card.image);
             cardElement.addEventListener('click', () => { this.pickUpCard(cardElement) });
-            if(this._numberOfColumns == 3) cardElement.classList.add('threeXfour');
-            else if(this._numberOfColumns == 4) cardElement.classList.add('fourXfour')
-    
-            board.appendChild(cardElement);
+            rowForCards.classList.add("row", "justify-content-center", "d-flex", "h-25");
+
+            if(this._numberOfColumns == 3){
+                rowForCards.appendChild(cardElement);
+                if((card.id+1)%3==0){
+                    board.appendChild(rowForCards);
+                    rowForCards = document.createElement("div");
+                }
+            }else if(this._numberOfColumns == 4){
+                rowForCards.appendChild(cardElement);
+                if((card.id+1)%4==0){
+                    board.appendChild(rowForCards);
+                    rowForCards = document.createElement("div");
+                }
+            }
 
         })
     }
@@ -58,15 +85,16 @@ export class MemoryGame {
     private pickUpCard = (cardElement: HTMLDivElement) => {
         // console.log(`id: ${cardElement.id}`);
         this._numberOfPickedCards++;
-        this._pickedCards.push({id: Number(cardElement.id)-1, pickedCard: cardElement});
-        cardElement.classList.add("picked");
+        this._pickedCards.push({id: Number(cardElement.id), pickedCard: cardElement});
+        cardElement.classList.add("averse");
         cardElement.classList.remove("reverse");
-        cardElement.style.backgroundColor = cardElement.getAttribute("name");
+        cardElement.style.backgroundImage = `url(./img/pepsi/${cardElement.getAttribute("name")})`;
         if(this._numberOfPickedCards >= 2) this.lockPicking();
 
     }
 
     private lockPicking = () => {
+
         const cards = document.querySelectorAll(".card");
 
         // zablokowanie możliwości podnoszenia
@@ -76,32 +104,35 @@ export class MemoryGame {
 
         this.checkPair();
     }
-// tutaj mamy problem
+
     private checkPair = () => {
         const cards: NodeListOf<HTMLDivElement> = document.querySelectorAll(".card");
 
-        if(this._pickedCards[0].pickedCard.style.backgroundColor == this._pickedCards[1].pickedCard.style.backgroundColor)
+        if(this._pickedCards[0].pickedCard.style.backgroundImage == this._pickedCards[1].pickedCard.style.backgroundImage)
         {
             // console.log('takie same');
                 setTimeout(() => {
+                    cards[this._pickedCards[0].id].style.backgroundImage = "none"; // usuwamy tło
                     cards[this._pickedCards[0].id].classList.add("guessed");
                     cards[this._pickedCards[0].id].classList.remove("reverse"); 
 
+                    cards[this._pickedCards[1].id].style.backgroundImage = "none"; // usuwamy tło
                     cards[this._pickedCards[1].id].classList.add("guessed");
                     cards[this._pickedCards[1].id].classList.remove("reverse");
                 }, 500)
         }
 
-
+        this.attemptIncrement();
     
-        setTimeout(() => {
+        setTimeout(() => 
+        {
             // sprawdzenie czy koniec gry - wszystkie pary odgadnięte
-            if(this.endGame()){ alert("koniec gry"); }
+            if(this.endGame()){ alert(`koniec gry, liczba prób: ${this._attempts}`); }
             else{
                 // odblokowanie możliwości klikania po 1 sek
                     cards.forEach(card => {
                         card.classList.remove('disabled');
-                        card.classList.remove("picked");
+                        card.classList.remove('averse');
                         if(!card.classList.contains("guessed")){
                             card.style.backgroundColor = 'rgb(206, 250, 241)';
                             card.classList.add("reverse");
@@ -138,28 +169,30 @@ export class MemoryGame {
         for(let i=0; i<this._numberOfColumns*this._numberOfRows; i++)
         {
             const card: Card = {
-                id: i+1,
-                color: this._colors[i],
-                flipped: false
+                id: i,
+                image: this._graphics[i],
+                obverse: false
             };
             cards.push(card);
         }
         return cards;
     }
 
-    
-    private drawColors(colorsAmount: number): string[]{
-        const colors: string[] = [];
+    private attemptIncrement(){
+        this._attempts++;
+        const scoreDiv: HTMLDivElement = document.querySelector("#score");
+        scoreDiv.innerHTML = `Attempts: ${this._attempts}`;
+    }
 
-        for(let i=0;i<colorsAmount;i++){
-            const colorHex = `#${Math.floor(Math.random()*16777215).toString(16)}`;
-            colors.push(colorHex);
-        }
-        return colors;
+    private blockOptionInputs(){
+        const optionInputs: NodeListOf<HTMLInputElement> = document.querySelectorAll("input");
+        optionInputs.forEach(input => {
+            input.disabled = !input.disabled;
+        });
     }
 
     private shuffleArray(){
-        this._colors.sort(() => Math.random() - 0.5);
+        this._graphics.sort(() => Math.random() - 0.5);
     }
 }
 
